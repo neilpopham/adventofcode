cbf0fa0915de9ad18633b9b8feb976819e48ef09
Neil Popham <neilpopham@gmail.com>
2020-12-07 22:07:26 +0000

    Python Day 1

---
+++ 2020/python/1.py
@@ -0,0 +1,37 @@
+import mylib
+
+def find_2(numbers):
+	l1 = 0
+	for n1 in numbers:
+		l2 = 0
+		for n2 in numbers:
+			n1 = int(n1)
+			n2 = int(n2)
+			if (n1 + n2 == 2020) and (l1 != l2):
+				print("%d + %d = 2020. %d x %d = %d" % (n1, n2, n1, n2, n1 * n2))
+				return
+			l2 = l2 + 1
+		l1 = l1 + 1
+
+def find_3(numbers):
+	l1 = 0
+	for n1 in numbers:
+		l2 = 0
+		for n2 in numbers:
+			l3 = 0
+			for n3 in numbers:	
+				n1 = int(n1)
+				n2 = int(n2)
+				n3 = int(n3)	
+				if (n1 + n2 + n3 == 2020) and (l1 != l2) and (l1 != l3) and (l2 != l3):	
+					print("%d + %d + %d = 2020. %d x %d x %d = %d" % (n1, n2, n3, n1, n2, n3, n1 * n2 * n3))	
+					return
+				l3 = l3 + 1	
+			l2 = l2 + 1
+		l1 = l1 + 1	
+
+numbers = mylib.load_data("1.txt")
+
+find_2(numbers)
+
+find_3(numbers)

5338313141a2ee31390888d9b2de6b31380a7850
Neil Popham <neilpopham@gmail.com>
2021-12-07 07:51:58 +0000

    Day 7
    Removed unused code

--- 2021/7.php
+++ 2021/7.php
@@ -1,18 +1,11 @@
 <?php
 
-function calc($n) {
-	$sum = 0;
-	for ($i = $n; $i >= 1; $i--) {
-		$sum = $sum + $i;
-	}
-	return $sum;
-}
-
 function parts($data, $func) {
 	sort($data);
 	$min = reset($data);
 	$max = end($data);
 	$count = array_fill($min, $max - $min + 1, 0);
+
 	for ($i = $min; $i <= $max; $i++) {
 		foreach ($data as $value) {
 			$count[$i] += $func(abs($value - $i));

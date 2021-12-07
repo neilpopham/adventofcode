76e1efbf0771cf709a66daadab4cb14be70cc504
Neil Popham <neilpopham@gmail.com>
2021-12-07 11:08:18 +0000

    Use min(<Array>)

--- 2021/7.php
+++ 2021/7.php
@@ -4,20 +4,15 @@ function parts($data, $func) {
 	sort($data);
 	$min = reset($data);
 	$max = end($data);
-	$count = array_fill($min, $max - $min + 1, 0);
+	$fuel = array_fill($min, $max - $min + 1, 0);
 
 	for ($i = $min; $i <= $max; $i++) {
 		foreach ($data as $value) {
-			$count[$i] += $func(abs($value - $i));
+			$fuel[$i] += $func(abs($value - $i));
 		}
 	}
-	$min = PHP_INT_MAX;
-	foreach ($count as $i => $fuel) {
-		if ($fuel < $min) {
-			$min = $fuel;
-		}
-	}
-	return $min;
+	
+	return min($fuel);
 }
 
 $raw = file_get_contents('data/7.txt');

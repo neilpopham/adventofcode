d26eeb2786907cb50ad7bc438019571a33e0ea92
Neil Popham <neilpopham@gmail.com>
2021-12-09 13:08:14 +0000

    Needed some help
    Worked fine with test data, but not real data. Was looking for X+1 not <X.

--- 2021/9.php
+++ 2021/9.php
@@ -2,9 +2,9 @@
 
 require('libs/core.php');
 
-function part1($data) {
+function get_lows($data) {
 	$offsets = [[0, -1], [1, 0], [0, 1], [-1, 0]];
-	$lows = 0;
+	$lows = [];
 	foreach ($data as $y => $row) {
 		foreach ($row as $x => $value) {
 			$low = true;
@@ -12,22 +12,28 @@ function part1($data) {
 				if (!isset($data[$y + $offset[1]])) {
 					continue;
 				}
-				$ovalue = $data[$y + $offset[1]][$x + $offset[0]] ?? 9;
-				if ($ovalue <= $value) {
+				$neighbour = $data[$y + $offset[1]][$x + $offset[0]] ?? 9;
+				if ($value > $neighbour) {
 					$low = false;
 					break;
-				}					
+				}
 			}
 			if ($low) {
-				$lows += ++$value;
+				$lows[] = [$x, $y, $value];
 			}
 		}
 	}
 	return $lows;
 }
 
-function test_item($x, $y, $data) {
-	$values[] = (int) $data[$y][$x];
+function make_key($x, $y) {
+	return 'P' . ($y * 1000 + $x);
+}
+
+function test_item($x, $y, $data, $done) {
+	$key = make_key($x, $y);
+	$values[$key] = 1;
+	$done[$key] = 1;
 	$offsets = [[0, -1], [1, 0], [0, 1], [-1, 0]];
 	foreach ($offsets as $o => $offset) {
 		$oy = $y + $offset[1];
@@ -35,62 +41,41 @@ function test_item($x, $y, $data) {
 			continue;
 		}
 		$ox = $x + $offset[0];
-		$value = $data[$oy][$ox] ?? PHP_INT_MAX;
-		if ($value == $values[0] + 1) {
-			$values = array_merge($values, test_item($ox, $oy, $data));
+		if (isset($done[make_key($ox, $oy)])) {
+			continue;
+		}
+		$neighbour = $data[$oy][$ox] ?? 9;
+		if (($neighbour != 9) && ($neighbour > $data[$y][$x])) {
+			$values = array_merge($values, test_item($ox, $oy, $data, $done));
 		}
 	}
 	return $values;
 }
 
-function part2($data) {
-	$offsets = [[0, -1], [1, 0], [0, 1], [-1, 0]];
-	$max = count($data[0]);
-	foreach ($data as $y => $row) {
-		$bowl = [];
-		foreach ($row as $x => $value) {
-			$matrix = test_item($x, $y, [$value], $data);
-			print_r($matrix);
-			
-			
-			/*
-			$bowl[] = [$x, $y, $value];
-			print "{$x},{$y}\n";
-			$d = [1, 1, 1, 1];
-			do {
-				$valid = false;
-				foreach ($offsets as $o => $offset) {
-					if ($d[$o] == 0) {
-						//print "d0\n";
-						continue;
-					}
-					$oy = $y + ($offset[1] * $d[$o]);
-					if (!isset($data[$oy])) {
-						//print "invalid oy {$oy}\n";
-						$d[$o] = 0;
-						continue;
-					}
-					$ox = $x + ($offset[0] * $d[$o]);
-					$ovalue = $data[$oy][$ox] ?? PHP_INT_MAX;
-					if ($ovalue == ($value + $d[$o])) {
-						print "{$x},{$y} -> {$ox},{$oy} ({$value} -> {$ovalue})\n";
-						$valid = true;
-						$bowl[] = [$ox, $oy, $ovalue];
-						$d[$o]++;
-					} else {
-						$d[$o] = 0;
-					}	
-				}				
-			} while($valid);
-			*/
+function part1($data) {
+	$lows = get_lows($data);
+	return array_reduce(
+		$lows,
+		function($total, $item) {
+			$total += $item[2] + 1;
+			return $total;
 		}
-		//print_r($bowl);
-		exit;		
-	}
-
+	);
 }
 
-$data = load_data("_9.txt");
+function part2($data) {
+	$offsets = [[0, -1], [1, 0], [0, 1], [-1, 0]];
+	$sums = [];
+	$lows = get_lows($data);
+	foreach ($lows as $low) {
+		list($x, $y, $value) = $low;
+		$sums[] = count(test_item($x, $y, $data, []));
+	}
+	rsort($sums);
+	return array_product(array_slice($sums, 0, 3));
+}
+
+$data = load_data("9.txt");
 
 foreach ($data as &$value) {
 	$value = str_split($value);

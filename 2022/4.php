87b5accdd1944947234dc430c298f85d6abacf44
Neil Popham <neilpopham@gmail.com>
2022-12-04 06:04:42 +0000

    PSR2

--- 2022/4.php
+++ 2022/4.php
@@ -6,7 +6,8 @@ $data = (new AdventOfCode())->input(4)->lines()->regex('/^(\d+)\-(\d+),(\d+)\-(\
 $total = 0;
 foreach ($data as $point) {
     if ((($point[0] >= $point[2]) && ($point[1] <= $point[3]))
-        || (($point[2] >= $point[0]) && ($point[3] <= $point[1]))) {
+        || (($point[2] >= $point[0]) && ($point[3] <= $point[1]))
+    ) {
         $total++;
     }
 }
@@ -14,10 +15,9 @@ print "{$total}\n";
 
 $total = 0;
 foreach ($data as $point) {
-    if (($point[2] > $point[1])
-        || ($point[3] < $point[0])
-        || ($point[0] > $point[3])
-        || ($point[1] < $point[2])) {
+    if (($point[2] > $point[1]) || ($point[3] < $point[0])
+        || ($point[0] > $point[3]) || ($point[1] < $point[2])
+    ) {
         continue;
     }
     $total++;

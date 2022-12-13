d11cf7cc8889144f166c48bf08106a43bdd37539
Neil Popham <neilpopham@gmail.com>
2022-12-13 20:49:02 +0000

    Day 13

--- 2022/13.php
+++ 2022/13.php
@@ -9,98 +9,49 @@ define('STATE_FAIL', 2);
 
 $packets = [];
 for ($i = 1; $i < count($data); $i += 3) {
-    $packets[] = [$data[$i - 1], $data[$i]];
+    $packets[] = [json_decode($data[$i - 1]), json_decode($data[$i])];
 }
 
-$parts = [];
-$parsed = [];
-foreach ($packets as $p => $packet) {
-    $parsed[$p] = [];
-    $parts[$p] = [];
-    foreach ($packet as $s => $side) {
-        $parts[$p][$s] = [];
-        $m = -1;
-        while (preg_match_all('/\[[^\]\[]*\]/', $side, $matches)) {
-            $matches = array_unique($matches[0]);
-            foreach ($matches as $match) {
-                $parts[$p][$s][++$m] = $match;
-                $side = str_replace($match, "#{$m}#", $side);
-            }
-        }
-        $parsed[$p][$s] = $side;
-    }
-}
-
-function process_lists($part, $lists)
+function compare($a, $b)
 {
-    $array = [];
-    foreach ($lists as $s => $list) {
-        if (preg_match('/^\[(.+)*\]$/', $list, $matches)) {
-            $array[$s] = isset($matches[1]) ? explode(',', $matches[1]) : [];
-        }
-    }
-
-    $left = current($array[0]);
-    $right = current($array[1]);
-
+    $left = current($a);
+    $right = current($b);
     $state = STATE_UNKNOWN;
     while ($state == STATE_UNKNOWN) {
         if ($left === false) {
             return $right === false ? STATE_UNKNOWN : STATE_SUCCESS;
-        } elseif ($right === false) {
+        }
+        if ($right === false) {
             return STATE_FAIL;
         }
-        if (preg_match('/^#(\d+)#$/', $left, $matches)) {
-            $left = $part[0][$matches[1]];
-            if (preg_match('/^#(\d+)#$/', $right, $matches)) {
-                $right = $part[1][$matches[1]];
-            } else {
-                $right = "[{$right}]";
-            }
-            $return = process_lists($part, [$left, $right]);
+        if (is_array($left) || is_array($right)) {
+            $left = is_array($left) ? $left : [$left];
+            $right = is_array($right) ? $right : [$right];
+            $return = compare($left, $right);
             if ($return != STATE_UNKNOWN) {
                 return $return;
             }
-        } elseif (preg_match('/^#(\d+)#$/', $right, $matches)) {
-            $right = $part[1][$matches[1]];
-            $left = "[{$left}]";
-            $return = process_lists($part, [$left, $right]);
-            if ($return != STATE_UNKNOWN) {
-                return $return;
-            }
-        } else {
-            if ($right > $left) {
-                return STATE_SUCCESS;
-            } elseif ($right < $left) {
-                return STATE_FAIL;
-            }
+        } elseif ($right > $left) {
+            return STATE_SUCCESS;
+        } elseif ($right < $left) {
+            return STATE_FAIL;
         }
-        $left = next($array[0]);
-        $right = next($array[1]);
+
+        $left = next($a);
+        $right = next($b);
     }
 }
 
 $total = 0;
-foreach ($parsed as $p => $packet) {
-    $lists = [];
-    foreach ($packet as $s => $side) {
-        if (preg_match('/^#(\d+)#$/', $side, $matches)) {
-            $i = $matches[1];
-            $lists[] = $parts[$p][$s][$i];
-        }
-    }
-
-    $state = process_lists($parts[$p], $lists);
-
-    if ($state == STATE_SUCCESS) {
+foreach ($packets as $p => $packet) {
+    if (compare($packet[0], $packet[1]) == STATE_SUCCESS) {
         $total += ($p + 1);
     }
 }
 
 print "{$total}\n";
 
-$packets[] = ['[[2]]', '[[6]]'];
-
+$packets[] = [[[2]], [[6]]];
 $packs = [];
 foreach ($packets as $p => $packet) {
     foreach ($packet as $s => $side) {
@@ -108,27 +59,8 @@ foreach ($packets as $p => $packet) {
     }
 }
 
-usort(
-    $packs,
-    function ($a, $b) {
-        $parts = [];
-        $parsed = [];
-        $lists = [];
-        foreach ([$a, $b] as $s => $side) {
-            $parts[$s] = [];
-            $m = -1;
-            while (preg_match_all('/\[[^\]\[]*\]/', $side, $matches)) {
-                $matches = array_unique($matches[0]);
-                foreach ($matches as $match) {
-                    $parts[$s][++$m] = $match;
-                    $side = str_replace($match, "#{$m}#", $side);
-                }
-            }
-            $parsed[$s] = $side;
-            $lists[$s] = $parts[$s][$m];
-        }
-        return process_lists($parts, $lists) == STATE_SUCCESS ? -1 : 1;
-    }
-);
+usort($packs, fn($a, $b) => (compare($a, $b) == STATE_SUCCESS ? -1 : 1));
 
-print (array_search('[[2]]', $packs) + 1) * (array_search('[[6]]', $packs) + 1) . "\n";
+$keys = array_filter($packs, fn($v) => in_array($v, [[[2]], [[6]]]));
+print array_reduce(array_keys($keys), fn($t, $v) => $t * ($v + 1), 1) . "\n";
+

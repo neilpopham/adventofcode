d7c13466d0dd5f81185a069338c69d026ae1e178
Neil Popham <neilpopham@gmail.com>
2022-12-13 21:21:44 +0000

    Day 6

--- 2019/6.php
+++ 2019/6.php
@@ -3,7 +3,16 @@ require 'libs/api.php';
 
 $data = (new AdventOfCode())->input(6)->lines()->regex('/(\w+)\)(\w+)/');
 
-print_r($data);
+// print_r($data);
+
+// $parents = [];
+// foreach ($data as $pairs) {
+//     if (!isset($orbits[$pairs[0]])) {
+//         $orbits[$pairs[0]] = [];
+//     }
+//     $orbits[$pairs[0]][$pairs[1]] = $pairs[1];
+// }
+// print_r($parents);
 
 $orbits = [];
 foreach ($data as $pairs) {
@@ -13,26 +22,30 @@ foreach ($data as $pairs) {
     $orbits[$pairs[0]][$pairs[1]] = $pairs[1];
 }
 
-print_r($orbits);
+// print_r($orbits);
 
-foreach ($orbits as $children) {
-    print_r($children);
-    foreach (array_keys($orbits) as $parent) {
-        print "{$parent}\n";
-        if (in_array($parent, $children)) {
-            print "found\n";
-            continue;
-        }
-    }
-    $root[] = $parent;
-}
-
-print "{$root}\n";
-
-
-function find_orbits()
+function find_tree($orbits, $parent, $path)
 {
-
+    $x = ["{$path}/{$parent}"];
+    if (false === isset($orbits[$parent])) {
+        return $x;
+    }
+    foreach ($orbits[$parent] as $child) {
+        $x = array_merge($x, find_tree($orbits, $child, $x[0]));
+        // print_r($x);
+        // exit;
+    }
+    return $x;
 }
 
-find_orbits();
+$tree = [];
+foreach (array_keys($orbits) as $parent) {
+    // print "{$parent}\n";
+    $tree = array_merge($tree, find_tree($orbits, $parent, ''));
+    // print_r($tree);
+}
+
+// print_r($tree);
+$tree = array_values(array_filter($tree, fn($t) => !preg_match('/^\/\w+$/', $t)));
+print_r($tree);
+exit;

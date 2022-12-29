541eaedc34489900f56f587e931983f6ac51d50a
Neil Popham <neilpopham@gmail.com>
2022-12-29 07:41:37 +0000

    Day 6

--- 2019/6.php
+++ 2019/6.php
@@ -3,17 +3,6 @@ require 'libs/api.php';
 
 $data = (new AdventOfCode())->input(6)->lines()->regex('/(\w+)\)(\w+)/');
 
-// print_r($data);
-
-// $parents = [];
-// foreach ($data as $pairs) {
-//     if (!isset($orbits[$pairs[0]])) {
-//         $orbits[$pairs[0]] = [];
-//     }
-//     $orbits[$pairs[0]][$pairs[1]] = $pairs[1];
-// }
-// print_r($parents);
-
 $orbits = [];
 foreach ($data as $pairs) {
     if (!isset($orbits[$pairs[0]])) {
@@ -22,30 +11,44 @@ foreach ($data as $pairs) {
     $orbits[$pairs[0]][$pairs[1]] = $pairs[1];
 }
 
-// print_r($orbits);
-
-function find_tree($orbits, $parent, $path)
+function process($parent, $count)
 {
-    $x = ["{$path}/{$parent}"];
-    if (false === isset($orbits[$parent])) {
-        return $x;
+    global $orbits, $g;
+    $g[$parent] = $count;
+    if (!isset($orbits[$parent])) {
+        return;
     }
     foreach ($orbits[$parent] as $child) {
-        $x = array_merge($x, find_tree($orbits, $child, $x[0]));
-        // print_r($x);
-        // exit;
+        process($child, $g[$parent] + 1);
     }
-    return $x;
 }
 
-$tree = [];
-foreach (array_keys($orbits) as $parent) {
-    // print "{$parent}\n";
-    $tree = array_merge($tree, find_tree($orbits, $parent, ''));
-    // print_r($tree);
+$g = [];
+process('COM', 0);
+
+print array_sum($g);
+print "\n";
+
+function process2($parent, $path)
+{
+    global $orbits, $g;
+    $g[$parent] = (empty($path) ? $path : "{$path}/") . $parent;
+    if (!isset($orbits[$parent])) {
+        return;
+    }
+    foreach ($orbits[$parent] as $child) {
+        process2($child, $g[$parent]);
+    }
 }
 
-// print_r($tree);
-$tree = array_values(array_filter($tree, fn($t) => !preg_match('/^\/\w+$/', $t)));
-print_r($tree);
-exit;
+$g = [];
+process2('COM', '');
+
+$you = explode('/', $g['YOU']);
+$san = explode('/', $g['SAN']);
+
+$diff1 = array_diff($san, $you);
+$diff2 = array_diff($you, $san);
+
+print count($diff1) - 1 + count($diff2) - 1;
+print "\n";

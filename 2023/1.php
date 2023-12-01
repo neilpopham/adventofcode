619090bddc2007c4b77cd39447f5c61f7a710400
Neil Popham <neilpopham@gmail.com>
2023-12-01 07:08:26 +0000

    Day 1

--- 2023/1.php
+++ 2023/1.php
@@ -2,12 +2,42 @@
 
 require 'libs/api.php';
 
-// Get live data
-// $input = (new AdventOfCode())->input(1);
-
-// Get example data
-$input = (new AdventOfCode())->load('_1');
+$input = (new AdventOfCode())->input(1);
 
 $data = $input->lines()->raw();
 
-print_r($data);
+$total = 0;
+foreach ($data as $line) {
+    $line = preg_replace('/[a-z]/', '', $line);
+    $number = $line[0] . $line[-1];
+    $total += $number;
+}
+print $total . "\n";
+
+$total = 0;
+foreach ($data as $line) {
+    $numbers = [
+        null,
+        '/(o)(n)(e)/',
+        '/(t)(w)(o)/',
+        '/(t)(hre)(e)/',
+        '/(f)(ou)(r)/',
+        '/(f)(iv)(e)/',
+        '/(s)(i)(x)/',
+        '/(s)(eve)(n)/',
+        '/(e)(igh)(t)/',
+        '/(n)(in)(e)/'
+    ];
+    $search = [];
+    $replace = [];
+    for ($n = 1; $n < count($numbers); $n++) {
+        $search[] = $numbers[$n];
+        $replace[] = '${1}' . $n . '${3}';
+    }
+    $search[] = '/[a-z]/';
+    $replace[] = '';
+    $line = preg_replace($search, $replace, $line);
+    $number = $line[0] . $line[-1];
+    $total += $number;
+}
+print $total . "\n";

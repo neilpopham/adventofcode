a027028bc7580cdbd357e45b0dd59fe90ca00ce1
Neil Popham <neilpopham@gmail.com>
2023-12-06 06:26:22 +0000

    Day 6

---
+++ 2023/6.php
@@ -0,0 +1,48 @@
+<?php
+
+require 'libs/api.php';
+
+$input = (new AdventOfCode())->input(6);
+
+$data = $input->lines()->regex('/\w+:\s+(.+)/');
+
+function numbers2array($value)
+{
+    return explode(' ', preg_replace('/\s+/', ' ', trim($value)));
+}
+
+$times = numbers2array($data[0][0]);
+$distances = numbers2array($data[1][0]);
+
+function find($distance, $time)
+{
+    $start = floor($time / 2);
+    $current = 0;
+    $total = 1;
+    $diff = 1;
+    while ($total > $current) {
+        $current = $total;
+        foreach ([$diff, -$diff] as $offset) {
+            if (($start + $offset) * ($time - ($start + $offset)) > $distance) {
+                $total++;
+            }
+        }
+        $diff++;
+    }
+    return $total;
+}
+
+$product = 1;
+foreach ($distances as $race => $distance) {
+    $time = $times[$race];
+    $product *= find($distance, $time);
+}
+print $product . "\n";
+
+$time = str_replace(' ', '', $data[0][0]);
+$distance = str_replace(' ', '', $data[1][0]);
+
+$s = microtime(true);
+print find($distance, $time) . "\n";
+$e = microtime(true);
+print $e - $s . "\n";

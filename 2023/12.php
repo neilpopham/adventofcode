1e1bc68c7a7c8fded1338f26bbd879400ee34c1b
Neil Popham <neilpopham@gmail.com>
2023-12-13 05:00:22 +0000

    Day 12 Part 1: Brute Force II

--- 2023/12.php
+++ 2023/12.php
@@ -13,40 +13,34 @@ foreach ($data as $key => $line) {
     }
     $data[$key][0] = str_replace(['.', '#'], ['0', '1'], $line[0]);
     $data[$key][1] = explode(',', $line[1]);
-
-    $chars = $data[$key][0];
-    $length = strlen($chars);
-
-    $zeros = bindec(str_replace('?', '1', $data[$key][0]));
-    $ones = bindec(str_replace('?', '0', $data[$key][0]));
-    $max = bindec(str_repeat('1', $length));
-    $max &= $zeros;
-
-    for ($n = 1; $n <= $max; $n++) {
-        $decimal = $n;
-        $decimal &= $zeros;
-        $decimal |= $ones;
-        $option = str_pad(decbin($decimal), $length, '0', STR_PAD_LEFT);
-        $data[$key][2][$option] = $option;
-    }
-    $data[$key][2] = array_keys($data[$key][2]);
-
-    $data[$key][3] = 0;
-    foreach ($data[$key][2] as $option) {
-        $parts = array_values(
-            array_map(
-                fn($v) => strlen($v),
-                array_filter(explode('0', $option))
-            )
-        );
-        if ($parts == $data[$key][1]) {
-            $data[$key][3]++;
+    if (preg_match_all('/\?/', $data[$key][0], $matches, PREG_OFFSET_CAPTURE)) {
+        $options = [];
+        $length = count($matches[0]);
+        $max = pow(2, $length);
+        for ($n = 0; $n < $max; $n++) {
+            $binary = str_pad(decbin($n), $length, '0', STR_PAD_LEFT);
+            $d = 0;
+            $options[$n] = $data[$key][0];
+            foreach ($matches[0] as $p) {
+                $options[$n][$p[1]] = $binary[$d];
+                $d++;
+            }
+        }
+        $data[$key][2] = 0;
+        foreach ($options as $option) {
+            $parts = array_values(
+                array_map(
+                    fn($v) => strlen($v),
+                    array_filter(explode('0', $option))
+                )
+            );
+            if ($parts == $data[$key][1]) {
+                $data[$key][2]++;
+            }
         }
     }
-
-    unset($data[$key][2]);
 }
 
-$total = array_reduce($data, fn($t, $v) => $t + $v[3], 0);
+$total = array_reduce($data, fn($t, $v) => $t + $v[2], 0);
 
 print $total . "\n";

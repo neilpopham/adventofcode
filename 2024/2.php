fe6af8c66cf54f94a55b3802015ef23a36d9c122
Neil Popham <neilpopham@gmail.com>
2024-12-02 07:20:18 +0000

    Day 2

--- 2024/2.php
+++ 2024/2.php
@@ -2,11 +2,49 @@
 
 require 'libs/api.php';
 
-$input = (new AdventOfCode())->input(1);
-$example = (new AdventOfCode())->example(1, 0);
+$input = (new AdventOfCode())->input(2);
+$data = $input->lines()->raw();
 
-$data = $example->lines()->raw();
+$reports = array_map(fn($report) => explode(' ', $report), $data);
 
-// $data = $input->lines()->raw();
+function check_levels($levels)
+{
+    $sgn = $levels[1] <=> $levels[0];
+    $count = count($levels);
+    for ($i = 1; $i < $count; $i++) {
+        $d = $levels[$i] - $levels[$i - 1];
+        $ad = abs($d);
+        if ($ad < 1 || $ad > 3) {
+            return false;
+        }
+        if (($levels[$i] <=> $levels[$i - 1]) !== $sgn) {
+            return false;
+        }
+    }
+    return true;
+}
 
-print_r($data);
+$total = 0;
+foreach ($reports as $levels) {
+    if (check_levels($levels)) {
+        $total++;
+    }
+}
+print $total . "\n";
+
+$total = 0;
+foreach ($reports as $levels) {
+    if (check_levels($levels)) {
+        $total++;
+        continue;
+    }
+    for ($i = 0; $i < count($levels); $i++) {
+        $partial = $levels;
+        array_splice($partial, $i, 1);
+        if (check_levels($partial)) {
+            $total++;
+            continue 2;
+        }
+    }
+}
+print $total . "\n";

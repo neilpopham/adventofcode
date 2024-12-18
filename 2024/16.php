3dd876ad0491acf85db841deffae5ecad50e8a8c
Neil Popham <neilpopham@gmail.com>
2024-12-18 18:19:02 +0000

    Day 16
    An updated version that works on both puzzle inputs.

--- 2024/16.php
+++ 2024/16.php
@@ -24,34 +24,27 @@ $offsets = [[0, -1], [1, 0], [0, 1], [-1, 0]];
 
 function check($item, $part1 = true)
 {
-    global $grid, $g, $offsets, $ex, $ey, $queue, $best;
-    $path = $item[0];
+    global $grid, $seen, $offsets, $ex, $ey, $queue, $best;
+    list($path, $g) = $item;
     list($x, $y, $d) = end($path);
-    $h = $item[1];
 
-    if ($h > $best) {
+    if ($best < $g) {
         return;
     }
 
     if ($x == $ex && $y == $ey) {
-        return $h;
+        return $g;
     }
 
-    if ($part1) {
-        if (isset($g[$y][$x])) {
-            if ($g[$y][$x] < $h) {
-                return;
-            }
+    if (isset($seen[$y][$x][$d])) {
+        if ($seen[$y][$x][$d] < $g) {
+            return;
         }
-        $g[$y][$x] = $h;
-    } else {
-        if (isset($g[$y][$x][$d])) {
-            if ($g[$y][$x][$d] < $h) {
-                return;
-            }
+        if ($part1 && ($seen[$y][$x][$d] == $g)) {
+            return;
         }
-        $g[$y][$x][$d] = $h;
     }
+    $seen[$y][$x][$d] = $g;
 
     foreach ($offsets as $o => $offset) {
         if (($o + 2) % 4 == $d) {
@@ -65,44 +58,41 @@ function check($item, $part1 = true)
             continue;
         }
 
-        $q = [$path, $h + ($o == $d ? 1 : 1001)];
+        $ng = $g + ($o == $d ? 1 : 1001);
+        $q = [$path, $ng];
         $q[0][] = [$nx, $ny, $o];
         $queue[] = $q;
     }
 }
 
-$g = [];
-$queue = [[[[$sx, $sy, 1]], 0]];
 $best = PHP_INT_MAX;
+$seen = [];
+$queue = [];
 $scores = [];
-$item = array_pop($queue);
+$successes = [];
+$item = [[[$sx, $sy, 1]], 0];
 while (!is_null($item)) {
     if (null !== $result = check($item)) {
-        $scores[] = $result;
-        sort($scores);
-        $best = reset($scores);
+        $scores[] = $item[1];
     }
     usort($queue, fn($a, $b) => $b[1] <=> $a[1]);
     $item = array_pop($queue);
 }
+sort($scores);
+$best = reset($scores);
 print $best . "\n";
 
-$g = [];
-$queue = [[[[$sx, $sy, 1]], 0]];
-$successes = [];
-$item = array_pop($queue);
+$seen = [];
+$queue = [];
+$unique = [];
+$item = [[[$sx, $sy, 1]], 0];
 while (!is_null($item)) {
     if (null !== $result = check($item, false)) {
-        $successes[] = $item[0];
+        foreach ($item[0] as $move) {
+            $unique["{$move[0]}|{$move[1]}"] = 1;
+        }
     }
     usort($queue, fn($a, $b) => $b[1] <=> $a[1]);
     $item = array_pop($queue);
-}
-
-$unique = [];
-foreach ($successes as $success) {
-    foreach ($success as $path) {
-        $unique["{$path[0]}|{$path[1]}"] = 1;
-    }
 }
 print count($unique) . "\n";

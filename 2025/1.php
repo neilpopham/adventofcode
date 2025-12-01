266aa7eef3e50378515fa2a7ad42eba0f3b9a8ee
Neil Popham <neilpopham@gmail.com>
2025-12-01 17:48:22 +0000

    Day 1

--- 2025/1.php
+++ 2025/1.php
@@ -2,7 +2,33 @@
 
 require 'libs/aoc.php';
 
-$input = new AdventOfCode\AdventOfCode()->input(1);
-$data = $input->raw();
+$input = new AdventOfCode\AdventOfCode()->input(1)->lines();
 
-print($data);
+$data = array_map(
+    fn ($x) => str_replace(['L', 'R'], ['-', ''], $x),
+    $input->raw()
+);
+
+$p = 50;
+$total = 0;
+foreach ($data as $value) {
+    $p = ($p + $value) % 100;
+    if ($p == 0) {
+        $total++;
+    }
+}
+print $total . "\n";
+
+$p = 50;
+$total = 0;
+foreach ($data as $value) {
+    $o = $p;
+    $p = (100 + $p + ($value % 100)) % 100;
+    $total += floor(abs($value) / 100);
+    if ($p == 0) {
+        $total++;
+    } elseif ($o != 0 && ($value > 0 && $p < $o || $value < 0 && $p > $o)) {
+        $total++;
+    }
+}
+print $total . "\n";

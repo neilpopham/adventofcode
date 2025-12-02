89c0cbd3a4f3e5fae2c6f070db3a1064dc749e0f
Neil Popham <neilpopham@gmail.com>
2025-12-02 16:44:23 +0000

    Day 2

--- 2025/2.php
+++ 2025/2.php
@@ -12,11 +12,7 @@ $data = array_map(
 $total = 0;
 foreach ($data as $values) {
     for ($value = $values[0]; $value <= $values[1]; $value++) {
-        if (strlen($value) % 2 == 1) {
-            continue;
-        }
-        $half = strlen($value) / 2;
-        if (substr($value, 0, $half) == substr($value, $half)) {
+        if (preg_match('/^(\d+)\1$/', $value)) {
             $total += $value;
         }
     }
@@ -26,14 +22,8 @@ print $total . "\n";
 $total = 0;
 foreach ($data as $values) {
     for ($value = $values[0]; $value <= $values[1]; $value++) {
-        $half = floor(strlen($value) / 2);
-        for ($x = 1; $x <= $half; $x++) {
-            $parts = str_split($value, $x);
-            $set = array_combine($parts, $parts);
-            if (count($set) == 1) {
-                $total += $value;
-                break;
-            }
+        if (preg_match('/^(\d+)\1+$/', $value)) {
+            $total += $value;
         }
     }
 }

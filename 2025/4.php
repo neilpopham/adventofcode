e39eb8bcaf6043594b86b5c771d974d773dd8458
Neil Popham <neilpopham@gmail.com>
2025-12-04 07:04:43 +0000

    Day 4
    Improved with cache

--- 2025/4.php
+++ 2025/4.php
@@ -8,27 +8,36 @@ $data = array_map(fn($x) => str_split($x), $input->raw());
 
 $grid = new AdventOfCode\Grid($data);
 
+$neighbours = new AdventOfCode\Grid();
+
+function neighbours($grid, $x, $y)
+{
+    $sides = 0;
+    for ($dy = -1; $dy <= 1; $dy++) {
+        for ($dx = -1; $dx <= 1; $dx++) {
+            if ($dx == 0 && $dy == 0) {
+                continue;
+            }
+            if ($grid->cell($x + $dx, $y + $dy, '.') == '@') {
+                $sides++;
+            }
+            if ($sides > 3) {
+                break 2;
+            }
+        }
+    }
+    return $sides;
+}
+
 $total = 0;
 for ($y = 0; $y < $grid->height; $y++) {
     for ($x = 0; $x < $grid->width; $x++) {
         if ($grid->cell($x, $y) == '@') {
-            $sides = 0;
-            for ($dy = -1; $dy <= 1; $dy++) {
-                for ($dx = -1; $dx <= 1; $dx++) {
-                    if ($dx == 0 && $dy == 0) {
-                        continue;
-                    }
-                    if ($grid->cell($x + $dx, $y + $dy, '.') == '@') {
-                        $sides++;
-                    }
-                    if ($sides > 3) {
-                        break 2;
-                    }
-                }
-            }
+            $sides = neighbours($grid, $x, $y);
             if ($sides < 4) {
                 $total++;
             }
+            $neighbours->set($x, $y, $sides);
         }
     }
 }
@@ -40,24 +49,18 @@ while ($total != $previous) {
     $previous = $total;
     for ($y = 0; $y < $grid->height; $y++) {
         for ($x = 0; $x < $grid->width; $x++) {
-            if ($grid->cell($x, $y) == '@') {
-                $sides = 0;
+            if ($neighbours->cell($x, $y, 9) < 4) {
+                $neighbours->set($x, $y, 9);
+                $grid->set($x, $y, 'X');
+                $total++;
                 for ($dy = -1; $dy <= 1; $dy++) {
                     for ($dx = -1; $dx <= 1; $dx++) {
-                        if ($dx == 0 && $dy == 0) {
-                            continue;
-                        }
-                        if ($grid->cell($x + $dx, $y + $dy, '.') == '@') {
-                            $sides++;
-                        }
-                        if ($sides > 3) {
-                            break 2;
+                        $cx = $x + $dx;
+                        $cy = $y + $dy;
+                        if ($grid->cell($cx, $cy, '.') == '@') {
+                            $neighbours->set($cx, $cy, neighbours($grid, $cx, $cy));
                         }
                     }
-                }
-                if ($sides < 4) {
-                    $total++;
-                    $grid->set($x, $y, 'X');
                 }
             }
         }

f41a120da0c7299fafbfdc71409d88558955a8fe
Neil Popham <neilpopham@gmail.com>
2025-12-06 13:37:07 +0000

    Day 6
    Took the hint and moved from right to left. Far easier.

--- 2025/6.php
+++ 2025/6.php
@@ -2,13 +2,16 @@
 
 require 'libs/aoc.php';
 
-$input = new AdventOfCode\AdventOfCode()->input(6)->lines();
+$input = new AdventOfCode\AdventOfCode()->input(6);
+
+$input->raw = rtrim($input->raw);
+$input = $input->lines(false);
 
 function dosum($sum, $operator)
 {
     $operations = [
-        'add' => fn(int $t, int $v): int => $t + $v,
-        'mul' => fn(int $t, int $v): int => $t * $v,
+        'add' => fn($t, $v) => $t + $v,
+        'mul' => fn($t, $v) => $t * $v,
     ];
     $start = $operator == '+' ? 0 : 1;
     $function = $operator == '+' ? 'add' : 'mul';
@@ -36,42 +39,23 @@ print $total . "\n";
 
 $data = $input->raw();
 
-$cols = [];
-$operators = str_split(end($data));
-foreach ($operators as $col => $value) {
-    if ($value == ' ') {
-        continue;
-    }
-    $cols[] = $col;
-}
-$max = 0;
-foreach ($data as $line) {
-    $max = max($max, strlen(substr($line, end($cols))));
-}
-$cols[] = count($operators) + $max;
-
-$sums = [];
-$operators = [];
-$maxrow = count($data) - 1;
-for ($c = 1; $c < count($cols); $c++) {
-    $c1 = $cols[$c - 1];
-    $c2 = $cols[$c];
-    $number = [];
-    foreach ($data as $row => $line) {
-        if ($row == $maxrow) {
-            $operators[] = $line[$c1];
-            break;
-        }
-        for ($i = $c1; $i < $c2 - 1; $i++) {
-            $number[$i] = ($number[$i] ?? '') . $line[$i];
-        }
-    }
-    $sums[] = $number;
-}
-
 $total = 0;
-foreach ($sums as $i => $sum) {
-    $operator = $operators[$i];
-    $total += dosum($sum, $operator);
+$max = strlen($data[0]) - 1;
+$numbers = [];
+for ($c = $max; $c >= 0; $c--) {
+    $numbers[$c] = '';
+    for ($r = 0; $r < count($data); $r++) {
+        $value = $data[$r][$c] ?? ' ';
+        if ($value == ' ') {
+            continue;
+        }
+        if (in_array($value, ['+', '*'])) {
+            $total += dosum($numbers, $value);
+            $numbers = [];
+            $c--;
+        } else {
+            $numbers[$c] .= $value;
+        }
+    }
 }
 print $total . "\n";

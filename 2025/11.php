321eec735ffa1ad94e8a8ed43e0ed289581f1039
Neil Popham <neilpopham@gmail.com>
2025-12-14 07:02:20 +0000

    Day 11
    Added caching, using the current node and whether that node had seen fft and dac as the unique key

--- 2025/11.php
+++ 2025/11.php
@@ -17,28 +17,36 @@ foreach ($data as $device) {
     $devices[$device[0]] = $device[1];
 }
 
-function follow($path)
+
+function follow($devices, $path, $fft, $dac)
 {
-    global $queue, $devices;
+    global $cache;
     if (empty($path)) {
         return;
     }
     $current = end($path);
+    $key = $current . $fft . $dac;
+    if (isset($cache[$key])) {
+        return $cache[$key];
+    }
+    if ($current == 'out') {
+        return $fft && $dac ? 1 : 0;
+    }
+    if ($current == 'fft') {
+        $fft = 1;
+    }
+    if ($current == 'dac') {
+        $dac = 1;
+    }
+    $cache[$key] = 0;
+    if (false === isset($devices[$current])) {
+        return 0;
+    }
     foreach ($devices[$current] as $device) {
         $new = array_merge($path, [$device]);
-        if ($device == 'out') {
-            return $new;
-        }
-        $queue[] = $new;
+        $cache[$key] += follow($devices, $new, $fft, $dac);
     }
+    return $cache[$key];
 }
 
-$total = 0;
-$queue = [['you']];
-do {
-    $path = array_pop($queue);
-    if ($route = follow($path)) {
-        $total++;
-    }
-} while ($path !== null);
-print $total . "\n";
+print follow($devices, ['svr'], 0, 0) . "\n";
